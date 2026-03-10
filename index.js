af185ddae02ae66ecafff3675210b1189c06a722
Tamires <tamiresalef1@gmail.com>
2026-03-10 12:53:18 -0300

    API de pedidos funcionando 100%

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+let pedidos = [
+  { id: 1, cliente: "Tamires Dev", produto: "Curso de Node", status: "Pendente" }
+];
 
 app.get('/', (req, res) => {
-  res.send('Servidor da Tamires funcionando!');
+  res.send('🏠 API da Tamires Online! Vá para /pedidos');
+});
+
+app.get('/pedidos', (req, res) => {
+  res.json(pedidos);
 });
 
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+  console.log('🚀 Servidor turbinado em http://localhost:3000');
 });
+
